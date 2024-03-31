a072e0d2d21a8941bada9807d1c584454f3fb4c2
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-03-31 16:30:42 +0100

    add cash and investments to networth

--- src/routes/finance/+page.server.ts
+++ src/routes/finance/+page.server.ts
@@ -3,6 +3,8 @@ export async function load({ fetch }) {
     let res = await fetch('http://localhost:8000/finance/networth');
     let data = await res.json();
     return {
+        cash: data.cash,
+        investments: data.investments,
         total: data.total,
     };
 }
