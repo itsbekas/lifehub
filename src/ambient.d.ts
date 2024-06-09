853bbd4b2ed7317fcc3336b8c59c4d6baadc87df
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-06-09 13:32:35 +0100

    feat: display t212 data

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -11,3 +11,20 @@ type Module = {
     type: string;
     providers: Array<Provider>;
 };
+
+type t212Transaction = {
+    timestamp: string;
+    amount: number;
+    type: string;
+    ticker: string;
+}
+
+type t212Data = {
+    balance: {
+        timestamp: string;
+        free: number;
+        invested: number;
+        result: number;
+    };
+    history: Array<t212Transaction>;
+}
