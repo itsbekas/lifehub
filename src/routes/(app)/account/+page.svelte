4821bef32762c8bfbc8263c5c49f26f7374ee5d7
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-07-17 13:40:52 +0100

    refactor: remove providers from accounts page (replaced in /settings/providers)

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-    import { Accordion, AccordionItem, Button, Heading, Listgroup, Modal, TabItem } from 'flowbite-svelte';
-    import AddProviderButton from './AddProviderButton.svelte';
     import AddModuleButton from './AddModuleButton.svelte';
-    import UserProviderCard from './UserProviderCard.svelte';
     import UserModuleCard from './UserModuleCard.svelte';
     import AccountAccordion from './AccountAccordion.svelte';
     /** @type {import('./$types').PageData}*/
-    export let data: { providers: Array<Provider>, modules: Array<Module>, userProviders: Array<Provider>, userModules: Array<Module> };
-    
-    let providersData = {
-        title: 'Providers',
-        category: 'provider',
-        cardComponent: UserProviderCard,
-        addButtonComponent: AddProviderButton,
-        userItems: data.userProviders,
-        items: data.providers
-    };
+    export let data: { modules: Array<Module>, userModules: Array<Module> };
+
     let modulesData = {
         title: 'Modules',
         category: 'module',
@@ -27,6 +16,5 @@
 </script>
 
 <div class="grid grid-cols-2 grid-rows-1 gap-10">
-    <AccountAccordion data={providersData} />
     <AccountAccordion data={modulesData} />
 </div>
