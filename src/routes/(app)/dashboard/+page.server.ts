9273ba02fc142e86a4d4150bee09ff9a28b983e9
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-08-19 04:32:04 +0100

    remove calendar from dashboard

--- src/routes/(app)/dashboard/+page.server.ts
+++ src/routes/(app)/dashboard/+page.server.ts
@@ -6,9 +6,6 @@ export async function load({ fetch }) {
     const t212Request = await fetch(api_url('/finance/trading212/data'));
     const t212Data = await t212Request.json();
 
-    const calendarRequest = await fetch(api_url('/calendar/events'));
-    const calendarData = await calendarRequest.json();
-
-    return { t212Data , calendarData };
+    return { t212Data };
 
 }
--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 
     import T212Widget from "$widgets/T212Widget.svelte";
-    import CalendarWidget from "$widgets/CalendarWidget.svelte";
 
     /** type {import('./$types').PageData} */
-    export let data: { t212Data: T212Data, calendarData: Array<CalendarEvent> };
+    export let data: { t212Data: T212Data };
 
 </script>
 
 <main class="mx-10 mt-5">
     <T212Widget data={data.t212Data} />
-    <CalendarWidget data={data.calendarData} />
 </main>
