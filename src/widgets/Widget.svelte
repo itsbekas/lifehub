825df50cd7145d5aa957a203c0cd495fabca1174
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-08-22 11:17:38 +0100

    fix: remove old pages and components using flowbite

--- src/widgets/Widget.svelte
+++ src/widgets/Widget.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 
-    import { Card } from 'flowbite-svelte';
-
     export let title: string;
     export let href: string;
 </script>
 
-<Card class="w-full" href={href}>
+<a class="w-full" href={href}>
     <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
     <slot />
-</Card>
+</a>
