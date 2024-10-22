b61f985e4b4ee396643c820f403f156bf22c4f18
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-10-23 00:26:21 +0100

    dockerize and switch from pnpm to npm

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,6 @@
+import { env } from '$env/dynamic/private'
+
 export const api_url = (path: string) => {
-    return `http://localhost:8000/api/v0${path}`;
+    const BACKEND_URL = env.BACKEND_URL;
+    return `http://${BACKEND_URL}:8000/api/v0${path}`;
 }
