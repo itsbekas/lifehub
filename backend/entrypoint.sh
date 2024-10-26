b857b317e211fa162d9c9f3f07ee0b8668fadcca
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-10-26 21:22:18 +0100

    bump python to version 3.13 and fix docker entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-uv venv .venv-dev
-source .venv-dev/bin/activate
-uv pip install -e .
+export UV_PROJECT_ENVIRONMENT=.venv-dev
+
+uv venv
+VIRTUAL_ENV="${UV_PROJECT_ENVIRONMENT}" uv pip install -e .
 
 exec uv run lifehub-api
