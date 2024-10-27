c5b8bb887d0c804577a8838a28d1a9f83c3ca81f
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-10-27 01:10:34 +0100

    add admin account and setup

--- backend/lifehub/config/checks.py
+++ backend/lifehub/config/checks.py
@@ -2,10 +2,12 @@ import time
 
 from sqlalchemy import create_engine
 
-from lifehub.config.constants import DATABASE_URL
+from lifehub.config.constants import ADMIN_PASSWORD, ADMIN_USERNAME, DATABASE_URL
 from lifehub.config.providers import setup_providers
 from lifehub.config.util.schemas import *  # noqa: F401,F403
 from lifehub.core.common.base.db_model import BaseModel
+from lifehub.core.common.database_service import Session, engine
+from lifehub.core.user.schema import User
 
 
 def check_mariadb() -> None:
@@ -14,7 +16,6 @@ def check_mariadb() -> None:
     start_time = time.time()
     while True:
         try:
-            engine = create_engine(DATABASE_URL)
             connection = engine.connect()
             connection.close()
             print("Successfully connected to MariaDB")
@@ -25,6 +26,29 @@ def check_mariadb() -> None:
                 exit(1)
             print("MariaDB not ready, waiting...")
             time.sleep(interval)
+
+
+def setup_admin_user() -> None:
+    user = User(
+        username=ADMIN_USERNAME,
+        email="admin@lifehub",
+        password=ADMIN_PASSWORD,
+        name="Admin",
+        verified=True,
+    )
+    with Session() as session:
+        # check if admin exists and update if necessary
+        current_user = (
+            session.query(User).filter(User.username == ADMIN_USERNAME).first()
+        )
+        if current_user:
+            current_user.email = user.email
+            current_user.password = user.password
+            current_user.name = user.name
+            current_user.verified = user.verified
+        else:
+            session.add(user)
+        session.commit()
 
 
 # Deprecated. Likely to be replaced with pre_run_setup()
@@ -41,4 +65,5 @@ def create_db_tables() -> None:
 def pre_run_setup() -> None:
     check_mariadb()
     create_db_tables()
+    setup_admin_user()
     setup_providers()
