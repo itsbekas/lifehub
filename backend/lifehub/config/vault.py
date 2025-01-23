f27c256c7de07193ff9fb82dbd1938dfdb1555a3
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2025-01-23 20:46:35 +0000

    setup vault db secrets

--- backend/lifehub/config/vault.py
+++ backend/lifehub/config/vault.py
@@ -1,6 +1,13 @@
 import hvac
 
-from lifehub.config.constants import VAULT_ADDR, VAULT_TOKEN
+from lifehub.config.constants import (
+    DB_HOST,
+    VAULT_ADDR,
+    VAULT_DB_PASSWORD,
+    VAULT_DB_USER,
+    VAULT_TOKEN,
+    DB_NAME
+)
 
 
 def setup_vault() -> None:
@@ -16,3 +23,43 @@ def setup_vault() -> None:
             path="transit",
         )
         print("Transit secrets engine enabled.")
+
+    # Enable the kv-v2 secret engine if not already enabled
+    kv_engines = sys_backend.list_mounted_secrets_engines()  # type: ignore
+    if "kv/" not in kv_engines:
+        client.sys.enable_secrets_engine(
+            backend_type="kv",
+            path="kv",
+            options={"version": "2"},
+        )
+        print("KV-v2 secrets engine enabled.")
+
+    # Enable the database secret engine if not already enabled
+    database_engines = sys_backend.list_mounted_secrets_engines()  # type: ignore
+    if "database/" not in database_engines:
+        client.sys.enable_secrets_engine(
+            backend_type="database",
+            path="database",
+        )
+        print("Database secrets engine enabled.")
+
+    # Configure the database secret engine 
+    client.secrets.database.configure(
+        name="mariadb",
+        plugin_name="mysql-database-plugin",
+        connection_url=f"{VAULT_DB_USER}:{VAULT_DB_PASSWORD}@tcp({DB_HOST}:3306)/",
+        allowed_roles="mariadb-role",
+    )
+
+    # Define the role
+    client.secrets.database.create_or_update_role(
+        name='mariadb-role',
+        db_name='mariadb',
+        creation_statements=[
+            "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';",
+            "GRANT SELECT ON *.* TO '{{name}}'@'%';",
+        ],
+        default_ttl='1h',
+        max_ttl='24h'
+    )
+
