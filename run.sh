eb5a239c14d451009b8e7d99cc6510ace416533c
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2025-03-20 15:02:51 +0000

    setup redis container and password

--- run.sh
+++ run.sh
@@ -7,7 +7,9 @@ export VAULT_APPROLE_ROLE_ID=$(vault read -field=role_id auth/approle/role/lifeh
 export VAULT_APPROLE_SECRET_ID=$(vault write -f -field=secret_id auth/approle/role/lifehub-app/secret-id)
 export SESSION_SECRET=$(vault kv get -field=session_secret kv/lifehub-secrets/frontend)
 
-rm -f backend/config.json
+# Setup Redis Password
+export REDIS_PASSWORD=$(openssl rand -base64 32)
+vault kv patch kv/lifehub/metadata redis_password=$REDIS_PASSWORD &> /dev/null
 
 # This is the production version of the script
 docker compose down
